10d39a79deb11c73070b0310e3b8993961bd48ab
sarvesh783421 <sarvesh.motlag@gmail.com>
2021-08-09 02:43:21 +0530

    [Sarvesh]Refractored : Code for UC-2(main)

---
+++ main/MoodAnalyser.java
@@ -0,0 +1,31 @@
+public class MoodAnaylizer {
+	
+	public static String MoodAnaylizer() {
+		String msg = null;
+		try {
+			if(msg.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+			
+		}catch(NullPointerException e) {
+			return "HAPPY";
+		}
+		
+	}
+
+	public static String MoodAnaylizer(String msg) {
+		try {
+			if(msg.contains("Sad"))
+				return "SAD";
+			else
+				return "HAPPY";
+			
+		}catch(NullPointerException e) {
+			return "HAPPY";
+		}
+	}
+
+	
+
+}
